b31f44b8452e06ee1a2b9ff38516a40efd1f8fbd
Steven Carlysle-Davies <S.Carlysle-Davies@epcc.ed.ac.uk>
2022-04-12 15:30:05 +0100

    Extract ModelDescription to own schema

--- src/model-output.ts
+++ src/model-output.ts
@@ -1,22 +1,24 @@
 import { ModelInput } from './model-input'
 
+export interface ModelDescription {
+  /**
+   * A short display name to identify the model
+   */
+  name: string
+  /**
+   * A version number identifying the version of the model used in this run.
+   */
+  modelVersion: string
+  /**
+   * A version number identifying the version of the connector used in this run.
+   * This may be the same as modelVersion if development of the two is closely-linked.
+   */
+  connectorVersion: string
+}
+
 export interface ModelOutput {
   metadata: ModelInput
-  model: {
-    /**
-     * A short display name to identify the model
-     */
-    name: string
-    /**
-     * A version number identifying the version of the model used in this run.
-     */
-    modelVersion: string
-    /**
-     * A version number identifying the version of the connector used in this run.
-     * This may be the same as modelVersion if development of the two is closely-linked.
-     */
-    connectorVersion: string
-  }
+  model: ModelDescription
   time: {
     /**
      * An ISO-8601 string encoding the date that each timeseries begins.
